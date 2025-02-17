726b73a350a34ab9aa7eacef2367d1958b28b451
unknown <zmzmvkvk@naver.com>
2025-02-17 16:43:26 +0900

    router 시작

---
+++ 5.Router/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <p>Home</p>,
+    errorElement: <p>Not Found🎈</p>,
+  },
+  {
+    path: "/videos",
+    element: <p>Videos</p>,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router}>asd</RouterProvider>;
+}
+
+export default App;
--- todo-final/.pnp.loader.mjs
+++ todo-final/.pnp.loader.mjs
@@ -1,13 +1,13 @@
-import { URL, fileURLToPath, pathToFileURL } from 'url';
-import fs from 'fs';
-import path from 'path';
-import moduleExports, { Module } from 'module';
+import { URL, fileURLToPath, pathToFileURL } from "url";
+import fs from "fs";
+import path from "path";
+import moduleExports, { Module } from "module";
 
 var PathType;
-(function(PathType2) {
-  PathType2[PathType2["File"] = 0] = "File";
-  PathType2[PathType2["Portable"] = 1] = "Portable";
-  PathType2[PathType2["Native"] = 2] = "Native";
+(function (PathType2) {
+  PathType2[(PathType2["File"] = 0)] = "File";
+  PathType2[(PathType2["Portable"] = 1)] = "Portable";
+  PathType2[(PathType2["Native"] = 2)] = "Native";
 })(PathType || (PathType = {}));
 const npath = Object.create(path);
 const ppath = Object.create(path.posix);
@@ -20,19 +20,19 @@ ppath.resolve = (...segments) => {
     return path.posix.resolve(ppath.cwd(), ...segments);
   }
 };
-const contains = function(pathUtils, from, to) {
+
+const contains = function (pathUtils, from, to) {
   from = pathUtils.normalize(from);
   to = pathUtils.normalize(to);
-  if (from === to)
-    return `.`;
-  if (!from.endsWith(pathUtils.sep))
-    from = from + pathUtils.sep;
+  if (from === to) return `.`;
+  if (!from.endsWith(pathUtils.sep)) from = from + pathUtils.sep;
   if (to.startsWith(from)) {
     return to.slice(from.length);
   } else {
     return null;
   }
 };
+
 npath.fromPortablePath = fromPortablePath;
 npath.toPortablePath = toPortablePath;
 npath.contains = (from, to) => contains(npath, from, to);
@@ -42,26 +42,19 @@ const UNC_WINDOWS_PATH_REGEXP = /^\/\/(\.\/)?(.*)$/;
 const PORTABLE_PATH_REGEXP = /^\/([a-zA-Z]:.*)$/;
 const UNC_PORTABLE_PATH_REGEXP = /^\/unc\/(\.dot\/)?(.*)$/;
 function fromPortablePath(p) {
-  if (process.platform !== `win32`)
-    return p;
+  if (process.platform !== `win32`) return p;
   let portablePathMatch, uncPortablePathMatch;
-  if (portablePathMatch = p.match(PORTABLE_PATH_REGEXP))
-    p = portablePathMatch[1];
-  else if (uncPortablePathMatch = p.match(UNC_PORTABLE_PATH_REGEXP))
-    p = `\\\\${uncPortablePathMatch[1] ? `.\\` : ``}${uncPortablePathMatch[2]}`;
-  else
-    return p;
+  if ((portablePathMatch = p.match(PORTABLE_PATH_REGEXP))) p = portablePathMatch[1];
+  else if ((uncPortablePathMatch = p.match(UNC_PORTABLE_PATH_REGEXP))) p = `\\\\${uncPortablePathMatch[1] ? `.\\` : ``}${uncPortablePathMatch[2]}`;
+  else return p;
   return p.replace(/\//g, `\\`);
 }
 function toPortablePath(p) {
-  if (process.platform !== `win32`)
-    return p;
+  if (process.platform !== `win32`) return p;
   p = p.replace(/\\/g, `/`);
   let windowsPathMatch, uncWindowsPathMatch;
-  if (windowsPathMatch = p.match(WINDOWS_PATH_REGEXP))
-    p = `/${windowsPathMatch[1]}`;
-  else if (uncWindowsPathMatch = p.match(UNC_WINDOWS_PATH_REGEXP))
-    p = `/unc/${uncWindowsPathMatch[1] ? `.dot/` : ``}${uncWindowsPathMatch[2]}`;
+  if ((windowsPathMatch = p.match(WINDOWS_PATH_REGEXP))) p = `/${windowsPathMatch[1]}`;
+  else if ((uncWindowsPathMatch = p.match(UNC_WINDOWS_PATH_REGEXP))) p = `/unc/${uncWindowsPathMatch[1] ? `.dot/` : ``}${uncWindowsPathMatch[2]}`;
   return p;
 }
 
@@ -73,13 +66,12 @@ function readPackageScope(checkPath) {
   do {
     separatorIndex = checkPath.lastIndexOf(npath.sep);
     checkPath = checkPath.slice(0, separatorIndex);
-    if (checkPath.endsWith(`${npath.sep}node_modules`))
-      return false;
+    if (checkPath.endsWith(`${npath.sep}node_modules`)) return false;
     const pjson = readPackage(checkPath + npath.sep);
     if (pjson) {
       return {
         data: pjson,
-        path: checkPath
+        path: checkPath,
       };
     }
   } while (separatorIndex > rootSeparatorIndex);
@@ -87,22 +79,20 @@ function readPackageScope(checkPath) {
 }
 function readPackage(requestPath) {
   const jsonPath = npath.resolve(requestPath, `package.json`);
-  if (!fs.existsSync(jsonPath))
-    return null;
+  if (!fs.existsSync(jsonPath)) return null;
   return JSON.parse(fs.readFileSync(jsonPath, `utf8`));
 }
 
 const [major, minor] = process.versions.node.split(`.`).map((value) => parseInt(value, 10));
-const HAS_CONSOLIDATED_HOOKS = major > 16 || major === 16 && minor >= 12;
-const HAS_UNFLAGGED_JSON_MODULES = major > 17 || major === 17 && minor >= 5 || major === 16 && minor >= 15;
-const HAS_JSON_IMPORT_ASSERTION_REQUIREMENT = major > 17 || major === 17 && minor >= 1 || major === 16 && minor > 14;
+const HAS_CONSOLIDATED_HOOKS = major > 16 || (major === 16 && minor >= 12);
+const HAS_UNFLAGGED_JSON_MODULES = major > 17 || (major === 17 && minor >= 5) || (major === 16 && minor >= 15);
+const HAS_JSON_IMPORT_ASSERTION_REQUIREMENT = major > 17 || (major === 17 && minor >= 1) || (major === 16 && minor > 14);
 
 async function tryReadFile(path2) {
   try {
     return await fs.promises.readFile(path2, `utf8`);
   } catch (error) {
-    if (error.code === `ENOENT`)
-      return null;
+    if (error.code === `ENOENT`) return null;
     throw error;
   }
 }
@@ -131,24 +121,19 @@ function getFileFormat(filepath) {
       throw new Error(`Unknown file extension ".wasm" for ${filepath}`);
     }
     case `.json`: {
-      if (HAS_UNFLAGGED_JSON_MODULES)
-        return `json`;
+      if (HAS_UNFLAGGED_JSON_MODULES) return `json`;
       throw new Error(`Unknown file extension ".json" for ${filepath}`);
     }
     case `.js`: {
       const pkg = readPackageScope(filepath);
-      if (!pkg)
-        return `commonjs`;
+      if (!pkg) return `commonjs`;
       return (_a = pkg.data.type) != null ? _a : `commonjs`;
     }
     default: {
-      if (entrypointPath !== filepath)
-        return null;
+      if (entrypointPath !== filepath) return null;
       const pkg = readPackageScope(filepath);
-      if (!pkg)
-        return `commonjs`;
-      if (pkg.data.type === `module`)
-        return null;
+      if (!pkg) return `commonjs`;
+      if (pkg.data.type === `module`) return null;
       return (_b = pkg.data.type) != null ? _b : `commonjs`;
     }
   }
@@ -156,12 +141,11 @@ function getFileFormat(filepath) {
 
 async function getFormat$1(resolved, context, defaultGetFormat) {
   const url = tryParseURL(resolved);
-  if ((url == null ? void 0 : url.protocol) !== `file:`)
-    return defaultGetFormat(resolved, context, defaultGetFormat);
+  if ((url == null ? void 0 : url.protocol) !== `file:`) return defaultGetFormat(resolved, context, defaultGetFormat);
   const format = getFileFormat(fileURLToPath(url));
   if (format) {
     return {
-      format
+      format,
     };
   }
   return defaultGetFormat(resolved, context, defaultGetFormat);
@@ -169,22 +153,19 @@ async function getFormat$1(resolved, context, defaultGetFormat) {
 
 async function getSource$1(urlString, context, defaultGetSource) {
   const url = tryParseURL(urlString);
-  if ((url == null ? void 0 : url.protocol) !== `file:`)
-    return defaultGetSource(urlString, context, defaultGetSource);
+  if ((url == null ? void 0 : url.protocol) !== `file:`) return defaultGetSource(urlString, context, defaultGetSource);
   return {
-    source: await fs.promises.readFile(fileURLToPath(url), `utf8`)
+    source: await fs.promises.readFile(fileURLToPath(url), `utf8`),
   };
 }
 
 async function load$1(urlString, context, nextLoad) {
   var _a;
   const url = tryParseURL(urlString);
-  if ((url == null ? void 0 : url.protocol) !== `file:`)
-    return nextLoad(urlString, context, nextLoad);
+  if ((url == null ? void 0 : url.protocol) !== `file:`) return nextLoad(urlString, context, nextLoad);
   const filePath = fileURLToPath(url);
   const format = getFileFormat(filePath);
-  if (!format)
-    return nextLoad(urlString, context, nextLoad);
+  if (!format) return nextLoad(urlString, context, nextLoad);
   if (HAS_JSON_IMPORT_ASSERTION_REQUIREMENT && format === `json` && ((_a = context.importAssertions) == null ? void 0 : _a.type) !== `json`) {
     const err = new TypeError(`[ERR_IMPORT_ASSERTION_TYPE_MISSING]: Module "${urlString}" needs an import assertion of type "json"`);
     err.code = `ERR_IMPORT_ASSERTION_TYPE_MISSING`;
@@ -193,7 +174,7 @@ async function load$1(urlString, context, nextLoad) {
   return {
     format,
     source: await fs.promises.readFile(filePath, `utf8`),
-    shortCircuit: true
+    shortCircuit: true,
   };
 }
 
@@ -201,21 +182,18 @@ const pathRegExp = /^(?![a-zA-Z]:[\\/]|\\\\|\.{0,2}(?:\/|$))((?:node:)?(?:@[^/]+
 const isRelativeRegexp = /^\.{0,2}\//;
 async function resolve$1(originalSpecifier, context, nextResolve) {
   var _a;
-  const {findPnpApi} = moduleExports;
-  if (!findPnpApi || isBuiltinModule(originalSpecifier))
-    return nextResolve(originalSpecifier, context, nextResolve);
+  const { findPnpApi } = moduleExports;
+  if (!findPnpApi || isBuiltinModule(originalSpecifier)) return nextResolve(originalSpecifier, context, nextResolve);
   let specifier = originalSpecifier;
   const url = tryParseURL(specifier, isRelativeRegexp.test(specifier) ? context.parentURL : void 0);
   if (url) {
-    if (url.protocol !== `file:`)
-      return nextResolve(originalSpecifier, context, nextResolve);
+    if (url.protocol !== `file:`) return nextResolve(originalSpecifier, context, nextResolve);
     specifier = fileURLToPath(url);
   }
-  const {parentURL, conditions = []} = context;
+  const { parentURL, conditions = [] } = context;
   const issuer = parentURL ? fileURLToPath(parentURL) : process.cwd();
   const pnpapi = (_a = findPnpApi(issuer)) != null ? _a : url ? findPnpApi(specifier) : null;
-  if (!pnpapi)
-    return nextResolve(originalSpecifier, context, nextResolve);
+  if (!pnpapi) return nextResolve(originalSpecifier, context, nextResolve);
   const dependencyNameMatch = specifier.match(pathRegExp);
   let allowLegacyResolve = false;
   if (dependencyNameMatch) {
@@ -233,20 +211,18 @@ async function resolve$1(originalSpecifier, context, nextResolve) {
   }
   const result = pnpapi.resolveRequest(specifier, issuer, {
     conditions: new Set(conditions),
-    extensions: allowLegacyResolve ? void 0 : []
+    extensions: allowLegacyResolve ? void 0 : [],
   });
-  if (!result)
-    throw new Error(`Resolving '${specifier}' from '${issuer}' failed`);
+  if (!result) throw new Error(`Resolving '${specifier}' from '${issuer}' failed`);
   const resultURL = pathToFileURL(result);
   if (url) {
     resultURL.search = url.search;
     resultURL.hash = url.hash;
   }
-  if (!parentURL)
-    setEntrypointPath(fileURLToPath(resultURL));
+  if (!parentURL) setEntrypointPath(fileURLToPath(resultURL));
   return {
     url: resultURL.href,
-    shortCircuit: true
+    shortCircuit: true,
   };
 }
 
@@ -254,25 +230,13 @@ const binding = process.binding(`fs`);
 const originalfstat = binding.fstat;
 const ZIP_MASK = 4278190080;
 const ZIP_MAGIC = 704643072;
-binding.fstat = function(...args) {
+binding.fstat = function (...args) {
   const [fd, useBigint, req] = args;
   if ((fd & ZIP_MASK) === ZIP_MAGIC && useBigint === false && req === void 0) {
     try {
       const stats = fs.fstatSync(fd);
-      return new Float64Array([
-        stats.dev,
-        stats.mode,
-        stats.nlink,
-        stats.uid,
-        stats.gid,
-        stats.rdev,
-        stats.blksize,
-        stats.ino,
-        stats.size,
-        stats.blocks
-      ]);
-    } catch {
-    }
+      return new Float64Array([stats.dev, stats.mode, stats.nlink, stats.uid, stats.gid, stats.rdev, stats.blksize, stats.ino, stats.size, stats.blocks]);
+    } catch {}
   }
   return originalfstat.apply(this, args);
 };
