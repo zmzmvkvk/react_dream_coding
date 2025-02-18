92440b6519f9d9b11d60eb2385781feb53d383bf
unknown <zmzmvkvk@naver.com>
2025-02-18 14:31:01 +0900

    query 마무리

--- 6.query/src/components/MainProducts.jsx
+++ 6.query/src/components/MainProducts.jsx
@@ -1,23 +1,31 @@
-import React, { useState } from 'react';
-import Products from './Products';
+import React, { useState } from "react";
+import Products from "./Products";
+import { v4 as uuidv4 } from "uuid";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function MainProducts() {
+  const id1 = uuidv4();
+  const id2 = uuidv4();
   const [showLeftProducts, setShowLeftProducts] = useState(true);
   const [showRightProducts, setShowRightProducts] = useState(true);
+  const client = useQueryClient();
   return (
-    <main className='container'>
+    <main className="container">
       <div>
-        {showLeftProducts && <Products />}
-        <button onClick={() => setShowLeftProducts((show) => !show)}>
-          Toggle
-        </button>
+        {showLeftProducts && <Products id={id1} />}
+        <button onClick={() => setShowLeftProducts((show) => !show)}>Toggle</button>
       </div>
       <div>
-        {showRightProducts && <Products />}
-        <button onClick={() => setShowRightProducts((show) => !show)}>
-          Toggle
-        </button>
+        {showRightProducts && <Products id={id2} />}
+        <button onClick={() => setShowRightProducts((show) => !show)}>Toggle</button>
       </div>
+      <button
+        onClick={() => {
+          client.invalidateQueries(["products"]);
+        }}
+      >
+        정보가 업데이트 되었음!
+      </button>
     </main>
   );
 }
--- 6.query/src/components/Products.jsx
+++ 6.query/src/components/Products.jsx
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
-import useProducts from '../hooks/use-products';
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Products() {
   const [checked, setChecked] = useState(false);
-  const [loading, error, products] = useProducts({ salesOnly: checked });
+  const {
+    isLoading,
+    error,
+    data: products,
+  } = useQuery({
+    queryKey: ["products", checked],
+    queryFn: async () => {
+      console.log("fetching...", checked);
+      return fetch(`data/${checked ? "sale_" : ""}products.json`).then((res) => res.json());
+    },
+    staleTime: 1000 * 60 * 5,
+  });
+
   const handleChange = () => setChecked((prev) => !prev);
 
-  if (loading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading...</p>;
 
   if (error) return <p>{error}</p>;
 
   return (
     <>
       <label>
-        <input type='checkbox' checked={checked} onChange={handleChange} />
+        <input type="checkbox" checked={checked} onChange={handleChange} />
         Show Only 🔥 Sale
       </label>
       <ul>
